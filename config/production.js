7121162232242ea02e7a63e47231075c84531784
niranjan.sovani <niranjan.sovani@globant.com>
2018-01-11 14:53:56 +0530

    changed db pwd

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports={
     db:{
-        connectString:'mongodb://ranjan:ranjan@78@mongodb/transport'
+        connectString:'mongodb://ranjan:ranjan78@mongodb/transport'
     },
     server:{
         port:8080
